daecd8ddc14d3b9af0696c2078a55e6c798e360e
Claudio <claudio.pereti96@gmail.com>
2021-04-18 19:24:06 +0200

    normalize and add white noise to image

---
+++ DAE/src/features/Processing.py
@@ -0,0 +1,25 @@
+
+import sys
+sys.path.append('../data/')
+from Loader import load_mnist
+import numpy as np
+
+
+def normalize(X):
+    """ return a numpy array normalized """
+
+    X = X/255.0
+    return X
+
+def add_white_noise(X):
+    """ return a numpy array with white noise added (normal distribution mean 0 var 1)"""
+    #distinguishes dataset of 28*28 image and 754 array
+
+    if len(X.shape) == 3:
+        noise = np.random.randn(X.shape[0],X.shape[1],X.shape[2])
+
+    if len(X.shape) == 2:
+        noise = np.random.randn(X.shape[0],X.shape[1])
+
+    X_noise = X+noise
+    return X_noise
